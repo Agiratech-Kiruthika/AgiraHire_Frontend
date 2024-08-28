f641588d55fb64d29b2151e4c3be104c30ef86df
MageshV <magesh.v@agiratech.com>
2024-08-28 16:34:40 +0530

    testing done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,7 +10,7 @@ const LoginPageWithContent = () => {
         <h1>Welcome to Our Internal Hiring Portal</h1>
         <p>
           Our Internal Hiring Portal is a comprehensive web-based solution designed to simplify and optimize your organization's internal hiring process. With intuitive features and
-          user-friendly interface, our portal empowers HR departments and hiring managers to efficiently manage internal job postings, applications, and candidate evaluations.
+          user-friendly interface, our portal empowers HR departments and hiring managers to efficiently manage internal job postings, applications, and candidate evaluations.fjehsjhdkj
         </p>
       </div>
       <div className="rightContent">
